8f0615a088e5ca1c5595023fe9f9ac38fc008339
zainul abidin <bidingurusmk@gmail.com>
2023-10-05 07:09:06 +0700

    data master

---
+++ lib/text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Home"),
+      ),
+      body: Text('Hello selamat datang'),
+    );
+  }
+}
