9358b58f310d1970eda1cd7da4e5e4db12cad6f0
zainul abidin <bidingurusmk@gmail.com>
2023-10-20 13:41:56 +0700

    singlechildscrollview

--- lib/container.dart
+++ lib/container.dart
@@ -10,22 +10,72 @@ class ContainerWidget extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Container"),
       ),
-      body: Column(
-        children: [
-          Container(
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "Button",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.pink[100],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text(
+                  "Send",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
-          ),
-          Text("Hello ini tombol saya"),
-        ],
+            Container(
+              margin: EdgeInsets.all(50),
+              height: 100,
+              width: 200,
+              alignment: Alignment.bottomCenter,
+              color: Colors.amber[900],
+              child: Text(
+                "Semangat Belajar",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xff7c94b6),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://media.istockphoto.com/id/455458707/id/vektor/buku-kecil-yang-bagus.jpg?s=1024x1024&w=is&k=20&c=Xua6lbb9NEnSwu1efLChqzxwVpbFrk3xYJMty-4JlBk='),
+                    fit: BoxFit.cover,
+                  ),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 8,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                height: 200,
+                width: 300,
+                margin: EdgeInsets.all(20)),
+            Container(
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: NetworkImage(
+                      'https://lh5.googleusercontent.com/WA2DnaPNyHSZmjyr8x3TNWcaxJazMpN5IMndGuCY-9fWJhzeDbI2_uAf7OlQEJfXBOz__YMq3YBDGFFJNS_NIlHgIP9X3DdUMCVuyzoSh6vDw1iKeih2dj2IOioB-s-rC9B29Xh8he3mBzEhBw'),
+                  fit: BoxFit.fitWidth,
+                ),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 8,
+                ),
+              ),
+              height: 200,
+              width: 300,
+              margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
+              transform: Matrix4.rotationZ(-0.1),
+            )
+          ],
+        ),
       ),
     );
   }
